f48368cdcc7db3b4be8cbc494eb7efcc9b96e2c3
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-08-14 22:49:43 -0700

    added the comments for the clarity

--- src/SumEvenGrandParent.java
+++ src/SumEvenGrandParent.java
@@ -1,3 +1,4 @@
+// Tree Node class
 class Node{
     int val;
     Node left;
@@ -14,12 +15,17 @@ class Node{
 
 public class SumEvenGrandParent {
 
-
+    //universal sum so that I can use it to be calculated in one and returned in another method
+    //I need clarity about why do i need to declare it again actual method of execution
     static int sum=0;
     static void rec(Node current, Node par, Node gpar){
+        // if its empty then no calculation can take place
         if(current == null){
             return;
         }
+        // if grandparent is not null and its even then add the value of current in sum
+        // then do the same on left and right side, of the tree, by making current as parent
+        // and parent as grandparent
         if(gpar != null && gpar.val%2 ==0){
             sum += current.val;
         }
