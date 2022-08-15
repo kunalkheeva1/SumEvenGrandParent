bed0902c4e28b30e436850ea4417305bd6f661f1
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-08-14 22:23:41 -0700

    coded the node class

--- src/SumEvenGrandParent.java
+++ src/SumEvenGrandParent.java
@@ -1,6 +1,20 @@
+class Node{
+    int val;
+    Node left;
+    Node right;
+    Node(int val, Node left, Node right){
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+}
+
+
+
 public class SumEvenGrandParent {
 
-    
+    static int sumEvenGrandParents()
 
 
 
